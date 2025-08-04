aea723fd1d47ca8b50a432ca882140c51f37bf04
pholiveira-dev <pedro.alves@escs.edu.br>
2025-08-04 20:31:46 -0300

    alterações recentes

--- aula4/testes_path.js
+++ aula4/testes_path.js
@@ -1,3 +1,4 @@
+const fs = require('fs').promises;
 const path = require('path');
 
 const caminhoCompleto = path.join(__dirname, 'pasta', 'msg.txt');
@@ -7,4 +8,4 @@ const nomeArquivo = path.basename('./aula4/pqp/msg.txt', '.txt');
 const extensao = path.extname('foto.png');
 
 const caminhoAbsoluto = path.resolve('aula5', 'msg.txt');
-console.log('Caminho absoluto: ', caminhoAbsoluto);
+
--- aula5/server.js
+++ aula5/server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('./', (req, res) => {
-    res.send('Seja muito bem-vindo(a)');
-})
-
-app.post('./contato', (req, res) => {
-    res.send('Agora funcionou?');
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
 })
 
 app.listen(3000, () => {
-    console.log('Para acessar o servidor: http://localhost:3000');
-    console.log('Acessando o servidor pela porta 3000');
+    console.log('Rodando o servidor...');
+    console.log('Servidor disponível em: http://localhost:3000');
 })
